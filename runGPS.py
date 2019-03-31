87a8504d1e6b7695741a21af0a345700694c2612
Theriley106 <christopherlambert106@gmail.com>
2019-03-31 06:44:23 -0700

    Updated all changes

---
+++ runGPS.py
@@ -0,0 +1,20 @@
+import json
+import os
+import random
+import time
+import requests
+
+
+lng = 74.4057
+lat = 40.0583
+while True:
+	lng += .001
+	lat += .001 if random.randint(1,2) == 2 else 0
+	url = "https://us-central1-hackathon-180117.cloudfunctions.net/function-1?long={}&lat={}".format(float(lng), float(lat))
+	print url
+	res = requests.get(url)
+	score = len(res.json()) + random.randint(25, 50) if random.randint(1,2) == 2 else len(res.json()) + random.randint(25, 50)
+	if int(score) == 0:
+		score = random.randint(25, 75)
+	os.system("""echo '{"lat": """ + str(lat) + """, "lng": -""" + str(lng) + """, "score": """ + str(score) + """}' > data.json""")
+	time.sleep(.1)
